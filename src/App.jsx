747a922eef0fc67947c9a59241d742a1395315f8
knighthot <fikramulbizli30@gmail.com>
2026-03-17 11:11:49 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,9 @@ export default function App() {
     <div style={{ width: '100%', height: '650px' }}>
       <DomeGallery 
         images={images} 
-        fit={0.8}
+        fit={0.4}
         minRadius={600}
+        imageBorderRadius="15px"
         maxVerticalRotationDeg={0}
         segments={34}
         dragDampening={2}
